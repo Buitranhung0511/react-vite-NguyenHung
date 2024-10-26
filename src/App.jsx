2e3ea9ceb75dc6f36866f2b4598df6ab52fa180b
Anh Ba Hung <buitranhung511@gmail.com>
2024-10-26 21:08:29 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+
+// () =>{}
+const App = () => {
   const [count, setCount] = useState(0)
+
+  // function myFunction(){
+  //   console.log(">>> Run My Function");
+  // }
+
+  const myFunction = () => {
+    console.log(">>> Run My Arrow Function")
+    
+  }
+  myFunction();
 
   return (
     <>
@@ -16,7 +28,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello World</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
